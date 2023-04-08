1a7339d19d5de3b113c0b2752bfff7b3519cdaa3
Spyynox <maximembiyakiaku@gmail.com>
2023-04-08 22:28:41 +0200

    update eslint

--- server.ts
+++ server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 const app = express();
+
+
+ff = 
+1
 app.listen(3000, () => {
-    console.log('server is listening on port 3000');
+    console.log('server is listening on port 3000')
 });
